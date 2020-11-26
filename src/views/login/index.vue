cd55a4509325ed92710625e588639b59857292b0
IronPillar-Sheng <junjieshenglogan@gmail.com>
2020-11-26 14:25:58 +0800

    修改登录页名称 删除登陆页的注册

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -3,7 +3,7 @@
   <el-form ref="loginForm" :model="loginForm" :rules="loginRules" class="login-form" auto-complete="on" label-position="left">
 
     <div class="title-container">
-      <h3 class="title">Login Form</h3>
+      <h3 class="title">MSOC演示</h3>
     </div>
 
     <el-form-item prop="username">
@@ -23,13 +23,13 @@
         </span>
     </el-form-item>
 
-    <el-button :loading="loading" type="primary" style="width:100%;margin-bottom:30px;" @click.native.prevent="handleLogin">Login</el-button>
+    <el-button :loading="loading" type="primary" style="width:100%;margin-bottom:30px;" @click.native.prevent="handleLogin">登陆</el-button>
 
-    <div class="tips">
-      <span style="margin-right:20px;">username: user/ser/csp</span>
-      <span> password: any</span>
-      <a style="float:right;" @click="toregister">注册</a>
-    </div>
+    <!--<div class="tips">-->
+      <!--<span style="margin-right:20px;">username: user/ser/csp</span>-->
+      <!--<span> password: any</span>-->
+      <!--<a style="float:right;" @click="toregister">注册</a>-->
+    <!--</div>-->
 
   </el-form>
 </div>
